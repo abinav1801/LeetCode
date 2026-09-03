7aae5ce1e8dcb044060a9feac5bf17b9b47c697a
Abinav <abinavdharunav@gmail.com>
2026-09-03 14:39:15 +0530

    Time: 0 ms (100%), Space: 45.3 MB (16.17%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,29 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        boolean odd = true, even = true;
-
-        for(int i = 0; i < n; i++){
-            if(nums1[i] % 2 == 1){
-                boolean valid = false;
-                for(int j = 0; j < n; j++){
-                    if(i != j && Math.abs(nums1[i] - nums1[j]) % 2 == 1){
-                        valid = true;
-                    }
-                }
-                if(!valid) odd = false;
-            }
-            else {
-                boolean valid = false;
-                for(int j = 0; j < n; j++){
-                    if(i != j && Math.abs(nums1[j] - nums1[i]) % 2 == 0){
-                        valid = true;
-                    }
-                }
-                if(!valid) even = false;
-            }
-        }
-
-        return odd | even;
+        return true;
     }
 }
